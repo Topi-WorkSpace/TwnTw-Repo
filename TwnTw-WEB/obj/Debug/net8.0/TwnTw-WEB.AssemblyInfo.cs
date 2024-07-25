81bf8c84c2e089b94a29a6a3a9cb9256468e11bd
PhanKydeptrai <kyp194490@gmail.com>
2024-07-25 21:36:34 +0700

    first commit

--- TwnTw-WEB/obj/Debug/net8.0/TwnTw-WEB.AssemblyInfo.cs
+++ TwnTw-WEB/obj/Debug/net8.0/TwnTw-WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d918fce3668bb624ea9f02638006a146a6c34b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
