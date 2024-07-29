6dd6f5c9a206eb2a641fbad6ae665539999735d3
Cee <chiennguyen2841998@gmail.com>
2024-07-29 21:12:49 +0700

    update cloud Db

--- TwnTw-WEB/obj/Debug/net8.0/TwnTw-WEB.AssemblyInfo.cs
+++ TwnTw-WEB/obj/Debug/net8.0/TwnTw-WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff5133e89dc1dabbb8ed8b11101738ad3008589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5f3ea23361c2058b490170cc03b8a417982554e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
