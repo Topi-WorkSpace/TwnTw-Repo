8e9d4a873a9e938dea8a43a8f9a2864d3c86779f
Cee <chiennguyen2841998@gmail.com>
2024-08-08 00:08:53 +0700

    update 08/08/2024

--- TwnTw-WEB/Models/TaskDetail.cs
+++ TwnTw-WEB/Models/TaskDetail.cs
@@ -7,10 +7,12 @@ namespace TwnTw_WEB.Models
         [Key, Required]
         public Guid TaskDetailId { get; set; } = Guid.NewGuid();
         public Guid UserId { get; set; }
+        public Guid WorkSpaceId { get; set; }
         public User? Users { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
 }
--- TwnTw-WEB/obj/Release/net8.0/TwnTw-WEB.AssemblyInfo.cs
+++ TwnTw-WEB/obj/Release/net8.0/TwnTw-WEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60d028c7e9c454e93838344cb6f0e50da6a3836d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0113519bbb623a8607a1d55efac79c27405557cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwnTw-WEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
